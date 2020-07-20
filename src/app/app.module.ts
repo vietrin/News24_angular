7a38b18c300afddb095e3f492bedd5a12168c6bb
vietrin <44705744+vietrin@users.noreply.github.com>
2020-07-20 14:53:29 +0700

    16h00 2020/07/20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,16 @@ import { NewsComponent } from './news/news.component';
 import { CategoryComponent } from './category/category.component';
 import { AccountComponent } from './account/account.component';
 import { CommentComponent } from './comment/comment.component';
+import { NewsListComponent } from './admin/news/news-list/news-list.component';
+import { UpdateNewsComponent } from './admin/news/update-news/update-news.component';
+import { CreateListComponent } from './admin/news/create-list/create-list.component';
+import { ListNewsComponent } from './admin/news/list-news/list-news.component';
+import { CreateNewsComponent } from './admin/news/create-news/create-news.component';
+import { ListCategoryComponent } from './admin/category/list-category/list-category.component';
+import { CreateCategoryComponent } from './admin/category/create-category/create-category.component';
+import { UpdateCategoryComponent } from './admin/category/update-category/update-category.component';
+import { ListCommentComponent } from './admin/comment/list-comment/list-comment.component';
+import { CreateCommentComponent } from './user/comment/create-comment/create-comment.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +24,17 @@ import { CommentComponent } from './comment/comment.component';
     NewsComponent,
     CategoryComponent,
     AccountComponent,
-    CommentComponent
+    CommentComponent,
+    NewsListComponent,
+    UpdateNewsComponent,
+    CreateListComponent,
+    ListNewsComponent,
+    CreateNewsComponent,
+    ListCategoryComponent,
+    CreateCategoryComponent,
+    UpdateCategoryComponent,
+    ListCommentComponent,
+    CreateCommentComponent
   ],
   imports: [
     BrowserModule,
